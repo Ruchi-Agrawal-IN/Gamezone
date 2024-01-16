fca2b0e7a7963b14d3a8d6d231b9badc98f96f0b
Ruchi Agrawal <ruchi@Ruchis-Mac-mini.local>
2024-01-16 19:35:17 +0530

    build a responsive layout

--- GameZone/src/App.tsx
+++ GameZone/src/App.tsx
@@ -1,10 +1,29 @@
 import "./App.css";
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
+import { Show } from "@chakra-ui/react";
 function App() {
   return (
     <>
-      <Button colorScheme="blue">Button</Button>
-      <Button colorScheme="green">Button</Button>
+      <Grid
+        h="100vh"
+        templateAreas={{
+          base: `'nav' 'main'`,
+          lg: `'nav nav' 'aside main'`,
+        }}
+        gap={4}
+      >
+        <GridItem area="nav" bg="tomato">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="papayawhip">
+            aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="tomato">
+          main
+        </GridItem>
+      </Grid>
     </>
   );
 }
