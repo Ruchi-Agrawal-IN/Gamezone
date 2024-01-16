ff9d600d62cc27c5fb40e246e4e1f6a5b407be25
Ruchi Agrawal <ruchi@Ruchis-Mac-mini.local>
2024-01-16 20:56:28 +0530

    build a colormode switch

--- GameZone/src/App.tsx
+++ GameZone/src/App.tsx
@@ -17,13 +17,9 @@ function App() {
           <NavBar />
         </GridItem>
         <Show above="lg">
-          <GridItem area="aside" bg="papayawhip">
-            aside
-          </GridItem>
+          <GridItem area="aside">aside</GridItem>
         </Show>
-        <GridItem area="main" bg="tomato">
-          main
-        </GridItem>
+        <GridItem area="main">main</GridItem>
       </Grid>
     </>
   );
