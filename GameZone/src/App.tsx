f02ab199635424c5a0020721e0d0ee9bbf1dc31a
Ruchi Agrawal <ruchi@Ruchis-Mac-mini.local>
2024-01-16 20:38:03 +0530

    built navbar

--- GameZone/src/App.tsx
+++ GameZone/src/App.tsx
@@ -1,19 +1,20 @@
 import "./App.css";
 import { Grid, GridItem } from "@chakra-ui/react";
 import { Show } from "@chakra-ui/react";
+import NavBar from "./components/navBar";
 function App() {
   return (
     <>
       <Grid
-        h="100vh"
+        // h="100vh"
         templateAreas={{
           base: `'nav' 'main'`,
           lg: `'nav nav' 'aside main'`,
         }}
         gap={4}
       >
-        <GridItem area="nav" bg="tomato">
-          Nav
+        <GridItem area="nav">
+          <NavBar />
         </GridItem>
         <Show above="lg">
           <GridItem area="aside" bg="papayawhip">
