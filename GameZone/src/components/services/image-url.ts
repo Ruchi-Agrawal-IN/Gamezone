a9b591d15b16a6d625af43d9ae3134c8719eb8cb
Ruchi Agrawal <ruchi.ggc@gmail.com>
2024-01-18 20:01:34 +0530

    handle games  with noimage

--- GameZone/src/components/services/image-url.ts
+++ GameZone/src/components/services/image-url.ts
@@ -1,5 +1,6 @@
+import noImage from "../../assets/no-image-placeholder-6f3882e0.webp";
 const getCroppedImageURL = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
   const target = "media/";
   const idx = url.indexOf(target) + target.length;
   return url.substring(0, idx) + "crop/600/400/" + url.substring(idx);
